ca38c10045d2011a56a90242da9a417e5f8e8d17
otogan <onurtogan10@gmail.com>
2020-03-12 11:59:17 -0500

    samples

--- Command Samples.sql
+++ Command Samples.sql
@@ -38,3 +38,56 @@ SELECT
 FROM employees e
 JOIN employees m ON e.reports_to = m.employee_id;
 -- Join Multiple Tables
+USE sql_store;
+SELECT 
+	o.order_id,
+    o.order_date,
+    c.first_name,
+    c.last_name,
+    os.name AS status
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN order_statuses os ON o.status = os.order_status_id;
+-- Exercise
+USE sql_invoicing;
+SELECT 
+	p.payment_id,
+    p.client_id,
+    c.name AS client_name,
+    p.invoice_id,
+    p.date,
+    p.amount,
+    pm.name AS payment_method
+FROM payments p
+JOIN clients c ON c.client_id = p.client_id
+JOIN payment_methods pm ON pm.payment_method_id = p.payment_method;
+-- LEFT JOIN
+SELECT 
+	p.product_id,
+    p.name,
+    oi.quantity AS ordered_quantity
+FROM products p
+LEFT JOIN order_items oi
+	ON p.product_id = oi.product_id;
+-- LEFT JOIN Multiple Tables
+SELECT
+	c.customer_id,
+    c.first_name,
+    o.order_id,
+    sh.name AS shipper
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh ON o.shipper_id = sh.shipper_id
+ORDER BY c.customer_id;
+-- Exercise
+USE sql_store;
+SELECT
+	o.order_id,
+    o.order_date,
+    c.first_name AS customer,
+    s.name AS shipper,
+    os.name AS status
+FROM orders o
+LEFT JOIN customers c ON o.customer_id = c.customer_id
+LEFT JOIN shippers s ON o.shipper_id = s.shipper_id
+LEFT JOIN order_statuses os ON o.status = os.order_status_id;
